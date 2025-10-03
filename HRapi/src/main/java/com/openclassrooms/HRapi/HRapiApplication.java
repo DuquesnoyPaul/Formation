b43d2b4254dc0b0dbc60cff961b6f805b1e833bd
DuquesnoyPaul <paulduquesnoy@outlook.fr>
2025-10-03 16:27:47 +0200

    feat(api-spring) : first commit API course

---
+++ HRapi/src/main/java/com/openclassrooms/HRapi/HRapiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.HRapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRapiApplication.class, args);
+	}
+
+}
---
+++ HRapi/src/test/java/com/openclassrooms/HRapi/HRapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.HRapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HRapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
